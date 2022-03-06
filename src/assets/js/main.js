ec52301ba4e4a79b4a9790ddf273fa2462df1ad3
im-fahad <abdullahalfahad.bd@gmail.com>
2022-03-06 20:48:53 +0600

    working on home page design

---
+++ src/assets/js/main.js
@@ -0,0 +1,13 @@
+// import $ from "jquery";
+// require('bootstrap/dist/js/bootstrap.bundle')
+// require('slick-carousel/slick/slick')
+//
+// $(document).ready(function () {
+//     // $('#locationSelect').select2();
+//
+//     $('#categorySlider').slick({
+//         infinite: true,
+//         slidesToShow: 4,
+//         slidesToScroll: 4
+//     });
+// });
